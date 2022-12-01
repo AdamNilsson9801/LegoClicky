6c908c204f85c34d18a3a9e1c7d2753555069b66
emmpe308 <115401685+emmpe308@users.noreply.github.com>
2022-12-01 14:39:08 +0100

    Update AboutUs.php

---
+++ AboutUs.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesheet/stil.css" media="screen" rel="stylesheet" type="text/css">
+    <title>Lego</title>
+  </head>
+	<body>
+
+    <h1>About us</h1>
+    
+      <form action="<?php $_SERVER['PHP_SELF']?>" method="get">     
+      </form>
+      <?php
+                                                      
+          print("<h3>About us
+
+          Lego Clicky is one of the leading websites with information about Lego constructions. With simple structure and clarity, we answer all questions about what is contained in different sets. Lego Clicky was founded by four students studying at Linköping University with a dream to facilitate Lego building.
+          
+          Lego Clicky was created in the fall semester 2022 at Liu, campus Norrköping. Four happy students were given a big project assignment where they had to create a website with information about lego constructions and decided that they would make it as good as they could.
+          
+          Our vision and our values ​​are that it should be easy, fun and educational to find out information about Lego pieces and Lego construction.
+          
+          Thank you for using Lego Clicky!
+          </h3>\n");                                 
+     
+      ?>
+    </body>
+</html>
